187c18cf536c285d824222b894af6e574571d1b1
imran <imufun2@gmail.com>
2021-01-20 15:33:03 +0600

    Initial Commit

---
+++ iOS-Project-Skeleton/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Project-Skeleton
+//
+//  Created by Imran on 20/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
